d69d7dc891a76262a6868ced65940f328613aab7
Alfonso <alfof87@gmail.com>
2020-06-12 17:46:48 +0200

    prova 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-//Titolo psw generator
-document .getElementById('text-js') .innerHTML="PASSWORD GENERATOR";
+
 //Chiedi all’utente il suo nome
 var name = prompt ("Qual é il tuo nome?");
 //Chiedi il suo cognome
 var surname = prompt("Qual é il tuo cognome?");
 //Chiedi il suo colore preferito
 var color = prompt("Qual é il tuo colore preferito?");
+//numero
+var number = 19;
+//Calcolo PASSWORD
+var password = name + surname + color + number;
+//Titolo psw generator
+document .getElementById('text-js') .innerHTML="La tua password: " + password;
